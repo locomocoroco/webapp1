6350126cf66aca11c78f7f89c9173ec1f84090cf
locomocoroco <31488172+locomocoroco@users.noreply.github.com>
2019-04-20 01:27:55 +0200

    execises done

--- simple/main.go
+++ simple/main.go
@@ -11,12 +11,27 @@ func home (w http.ResponseWriter,r *http.Request) {
 }
 func contact (w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type","text/html")
-	fmt.Fprint(w, "to get in touch <ahref=\"mailto:lol@loc.com\">NilsW</a>")
+	fmt.Fprint(w, "to get in touch <a href=\"mailto:lol@loc.com\">NilsW</a>")
+}
+func faq (w http.ResponseWriter,r *http.Request) {
+	w.Header().Set("Content-Type","text/html")
+	fmt.Fprint(w, "<h1>Here are some of the most asked Questions.</h1>")
+	fmt.Fprint(w, "This that and this and that")
+}
+
+func notFound (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type","text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Whoops something went wrong here.</h1>")
+	fmt.Fprint(w, "get in touch if error persists <a href=\"mailto:lol@loc.com\">lol@loc.com</a>")
 }
 func main() {
 	r:=mux.NewRouter()
+	r.NotFoundHandler=http.HandlerFunc(notFound)
 	r.HandleFunc("/",home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq",faq)
 	http.ListenAndServe(":3000", r)
+
 
 }

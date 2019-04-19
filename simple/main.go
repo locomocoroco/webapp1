a57cabdcf47d74613d626a5461b7857dd9006104
locomocoroco <31488172+locomocoroco@users.noreply.github.com>
2019-04-19 23:50:56 +0200

    Gorilla_mux

--- simple/main.go
+++ simple/main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
-
-func handkeFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome! site</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "to get in touch <ahref=\"mailto:lol@loc.com\">myemail</a>")
-	}
+func home (w http.ResponseWriter,r *http.Request) {
+	w.Header().Set("Content-Type","text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome! site</h1>")
 }
-
+func contact (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type","text/html")
+	fmt.Fprint(w, "to get in touch <ahref=\"mailto:lol@loc.com\">NilsW</a>")
+}
 func main() {
-	http.HandleFunc("/", handkeFunc)
-	http.ListenAndServe(":3000", nil)
+	r:=mux.NewRouter()
+	r.HandleFunc("/",home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 
 }

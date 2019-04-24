f1a195d7c68cadbf152f902cd84ba87c66403ba9
locomocoroco <31488172+locomocoroco@users.noreply.github.com>
2019-04-24 23:34:17 +0200

    first view

--- simple/main.go
+++ simple/main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
 )
+
+var homeTemplate *template.Template
+
 func home (w http.ResponseWriter,r *http.Request) {
 	w.Header().Set("Content-Type","text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome! site</h1>")
+
+	if err:=homeTemplate.Execute(w,nil); err!=nil {
+		panic(err)
+	}
 }
 func contact (w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type","text/html")
 	fmt.Fprint(w, "to get in touch <a href=\"mailto:lol@loc.com\">NilsW</a>")
 }
 func main() {
+	var err error
+	homeTemplate, err=template.ParseFiles("../views/home.gohtml")
+	if err !=nil {
+		panic(err)
+	}
 	r:=mux.NewRouter()
 	r.HandleFunc("/",home)
 	r.HandleFunc("/contact", contact)

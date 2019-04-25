19b1e987c32e48cb44e015a454895da4fff19044
locomocoroco <31488172+locomocoroco@users.noreply.github.com>
2019-04-26 00:31:45 +0200

    refactor

--- views/view.go
+++ views/view.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"html/template"
+	"net/http"
 	"path/filepath"
 )
 
@@ -27,6 +28,9 @@ type View struct {
 	Layout   string
 }
 
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
 func layoutFiles() []string {
 	files, err := filepath.Glob(LayoutDir + "*" + TemplareExt)
 	if err != nil {

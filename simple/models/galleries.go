27575167293a4b0ed30853f73b845ca452a1159f
locomocoroco <31488172+locomocoroco@users.noreply.github.com>
2019-05-05 01:18:20 +0200

    model gallery added

---
+++ simple/models/galleries.go
@@ -0,0 +1,18 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+type Gallery struct {
+	gorm.Model
+	UserID uint   `gorm:"not null;index"`
+	Title  string `gorm:"not null"`
+}
+type GalleryService interface {
+	GalleryDB
+}
+type GalleryDB interface {
+	Create(gallery *Gallery) error
+}
+type galleryGorm struct {
+	dn *gorm.DB
+}
